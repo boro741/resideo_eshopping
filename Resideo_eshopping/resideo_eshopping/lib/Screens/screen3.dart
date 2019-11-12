3071dc4a7b1ef91dc828c2a13e77a57ed294c465
Pavan Lal Boro <pavan.boro@resideo.com>
2019-11-12 12:59:30 +0530

    dialog box added

---
+++ Resideo_eshopping/resideo_eshopping/lib/Screens/screen3.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class Screen3 extends StatelessWidget {
+  final GlobalKey<FormState> _formKeyValue = new GlobalKey<FormState>();
+
+  // Estimated cost of product to be sent from the previous page
+  Screen3(@required this.estimatedCost);
+  final double estimatedCost;
+
+  showAlertDialog(BuildContext context) {
+    // set up the buttons
+    Widget cancelButton = FlatButton(
+      child: Text("No"),
+      onPressed: () {
+        //Todo: Do Something
+      },
+    );
+    Widget continueButton = FlatButton(
+      child: Text("Yes"),
+      onPressed: () {
+        //Todo: Do Something
+      },
+    );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text("Order Confrimation"),
+      content: Text("Do you want to proceed?"),
+      actions: [
+        cancelButton,
+        continueButton,
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          alignment: Alignment.center,
+          child: Text("Add User Details",
+              style: TextStyle(
+                color: Colors.white,
+              )),
+        ),
+      ),
+      body: Form(
+        key: _formKeyValue,
+        autovalidate: true,
+        child: new ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0),
+          children: <Widget>[
+            TextFormField(
+              decoration: const InputDecoration(
+                icon: const Icon(
+                  FontAwesomeIcons.userCircle,
+                  color: Color(0xff11b719),
+                ),
+                hintText: 'Enter your Name',
+                labelText: 'Name',
+              ),
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                icon: const Icon(
+                  FontAwesomeIcons.phone,
+                  color: Color(0xff11b719),
+                ),
+                hintText: 'Ex: +91-9700000000',
+                labelText: 'Phone',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            TextFormField(
+              maxLines: 3,
+              decoration: const InputDecoration(
+                icon: const Icon(
+                  FontAwesomeIcons.home,
+                  color: Color(0xff11b719),
+                ),
+                labelText: 'Address',
+              ),
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                icon: const Icon(
+                  FontAwesomeIcons.mapMarker,
+                  color: Color(0xff11b719),
+                ),
+                hintText: 'Ex: 500000',
+                labelText: 'Zip Code',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.all(20.0),
+                  child: Text(
+                    'Estimated Price: ',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Color(0xff11b719)),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(20.0),
+                  child: Text(
+                    'Rs. $estimatedCost',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 200.0,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                RaisedButton(
+                    color: Color(0xff11b719),
+                    textColor: Colors.white,
+                    child: Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Text("Proceed", style: TextStyle(fontSize: 24.0)),
+                          ],
+                        )),
+                    onPressed: () {
+                      showAlertDialog(context);
+                    },
+                    shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(30.0))),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- Resideo_eshopping/resideo_eshopping/lib/main.dart
+++ Resideo_eshopping/resideo_eshopping/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:resideo_eshopping/Screens/screen3.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,106 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Color(0xff11b719),
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final GlobalKey<FormState> _formKeyValue = new GlobalKey<FormState>();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Container(
-          alignment: Alignment.center,
-          child: Text("Screen 3",
-              style: TextStyle(
-                color: Colors.white,
-              )),
-        ),
-      ),
-      body: Form(
-        key: _formKeyValue,
-        autovalidate: true,
-        child: new ListView(
-          padding: const EdgeInsets.symmetric(horizontal: 15.0),
-          children: <Widget>[
-            TextFormField(
-              decoration: const InputDecoration(
-                icon: const Icon(
-                  FontAwesomeIcons.userCircle,
-                  color: Color(0xff11b719),
-                ),
-                hintText: 'Enter your Name',
-                labelText: 'Name',
-              ),
-            ),
-            TextFormField(
-              decoration: const InputDecoration(
-                icon: const Icon(
-                  FontAwesomeIcons.phone,
-                  color: Color(0xff11b719),
-                ),
-                hintText: 'Ex: +91-9700000000',
-                labelText: 'Phone',
-              ),
-              keyboardType: TextInputType.number,
-            ),
-            TextFormField(
-              maxLines: 3,
-              decoration: const InputDecoration(
-                icon: const Icon(
-                  FontAwesomeIcons.home,
-                  color: Color(0xff11b719),
-                ),
-                labelText: 'Address',
-              ),
-            ),
-            TextFormField(
-              decoration: const InputDecoration(
-                icon: const Icon(
-                  FontAwesomeIcons.mapMarker,
-                  color: Color(0xff11b719),
-                ),
-                hintText: 'Ex: 500000',
-                labelText: 'Zip Code',
-              ),
-              keyboardType: TextInputType.number,
-            ),
-            SizedBox(
-              height: 150.0,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                RaisedButton(
-                    color: Color(0xff11b719),
-                    textColor: Colors.white,
-                    child: Padding(
-                        padding: EdgeInsets.all(10.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            Text("Proceed", style: TextStyle(fontSize: 24.0)),
-                          ],
-                        )),
-                    onPressed: () {
-
-                    },
-                    shape: new RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(30.0))),
-              ],
-            ),
-          ],
-        ),
-      ),
+      home: Screen3(0.0),
     );
   }
 }

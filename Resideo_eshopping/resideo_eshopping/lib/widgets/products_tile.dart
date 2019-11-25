c264d3bdda206dda60e2bb54c4ee1b916391e495
VandanaBaidya <vandanabaidya@gmail.com>
2019-11-25 10:55:57 +0530

    added animation to page navigator method

--- Resideo_eshopping/resideo_eshopping/lib/widgets/products_tile.dart
+++ Resideo_eshopping/resideo_eshopping/lib/widgets/products_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:resideo_eshopping/Screens/product_detail.dart';
 import 'package:resideo_eshopping/model/product.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -8,11 +9,13 @@ class ProductsTile extends StatelessWidget {
   Product _products;
   //Product pd;
   ProductsTile(this._products);
+  Widget widget;
 
   @override 
   Widget build(BuildContext context) { 
   void navigateToProductdetail(Product pd) async{
-  Navigator.push(context, MaterialPageRoute(builder: (context)=> ProductDetail(pd)));
+  //Navigator.push(context, MaterialPageRoute(builder: (context)=> ProductDetail(pd)));
+  Navigator.push(context, ScaleRoute(page: ProductDetail(pd)));
   }
     return Column(
       children: <Widget>[
@@ -20,28 +23,27 @@ class ProductsTile extends StatelessWidget {
           color: (_products.quantity != 0 ? Color.fromRGBO(255, 255, 255, 1.0) : Color.fromRGBO(255, 255, 255, 0.1)),
           child: ListTile(
             isThreeLine: true,
-            title: Text(_products.title),
+            title: PlatformText(_products.title),
             subtitle:
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Text(_products.sDesc),
+                PlatformText(_products.sDesc),
                 Row(
                   children: <Widget>[
                     Icon(FontAwesomeIcons.rupeeSign, size: 10,),
-                    Text((_products.price).toString()),
+                    PlatformText((_products.price).toString()),
                   ]   
                 ),
               ],
             ),
             onTap: (){ navigateToProductdetail(_products);},
+            
             leading: 
-            //showThumbnail(_products.quantity),
             Container(
               margin: EdgeInsets.only(left: 6.0),
               child: Image.network(_products.thumbnail, height: 50.0, fit: BoxFit.fill,),
             ),
-            
           ),
         ),
         //Divider()
@@ -49,9 +51,9 @@ class ProductsTile extends StatelessWidget {
     );
   }
 
-  /*
+/*  
   Widget showThumbnail(int quantity){
-    if(quantity == 0){
+    if(quantity != 0){
       return Container(
         child: Stack(
           children: <Widget>[
@@ -75,6 +77,37 @@ class ProductsTile extends StatelessWidget {
               child: Image.network(_products.thumbnail, height: 50.0, fit: BoxFit.fill,),
             );
     }
-  }*/
-  
+  }
+  */
+}
+
+class ScaleRoute extends PageRouteBuilder {
+  final Widget page;
+  ScaleRoute({this.page})
+      : super(
+          pageBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+          ) =>
+              page,
+          transitionsBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+            Widget child,
+          ) =>
+              ScaleTransition(
+                scale: Tween<double>(
+                  begin: 0.0,
+                  end: 1.0,
+                ).animate(
+                  CurvedAnimation(
+                    parent: animation,
+                    curve: Curves.fastOutSlowIn,
+                  ),
+                ),
+                child: child,
+              ),
+        );
 }

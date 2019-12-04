810776e5b9f7dbdbfcc9e70637a964b97d98b67d
VandanaBaidya <vandanabaidya@gmail.com>
2019-12-04 15:54:50 +0530

    Added navigation drawer to the screen

--- Resideo_eshopping/resideo_eshopping/lib/Screens/product_list_page.dart
+++ Resideo_eshopping/resideo_eshopping/lib/Screens/product_list_page.dart
@@ -3,11 +3,9 @@ import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:resideo_eshopping/model/product.dart';
-import 'package:resideo_eshopping/repository/products_repository.dart';
-import 'package:resideo_eshopping/util/dbhelper.dart';
-import 'package:resideo_eshopping/widgets/drawer.dart';
 import 'package:resideo_eshopping/controller/product_controller.dart';
 import 'package:resideo_eshopping/widgets/products_tile.dart';
 
@@ -23,16 +21,16 @@ class _ProductsListPageState extends State<ProductsListPage>
     with SingleTickerProviderStateMixin {
   ProductController productController=ProductController();
   String dropdownValue = 'Categories';
-  List<Product> _products = <Product>[];
   List<Product> currentList = <Product>[];
-  String _currentlySelected = "All";
   bool _isProgressBarShown = true;
   AnimationController controller;
   Animation<double> animation;
+  
 
-
-  void getProducts(String value){
-  productController.getProductList(value).then((result){setState((){currentList=result;});});
+  getProduct(String value){
+  productController.getProductList(value).then((result){setState((){currentList=result;
+  _isProgressBarShown = false;
+  });});
   }
   
   @override
@@ -63,36 +61,87 @@ class _ProductsListPageState extends State<ProductsListPage>
 
     return Scaffold(
       key: key,
-      drawer: AppDrawer(),
+      drawer: Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          Container(
+                child: UserAccountsDrawerHeader(
+                  accountName: Text("Name"),
+                  accountEmail: Text("abc@gmail.com"),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundColor:
+                        Theme.of(context).platform == TargetPlatform.iOS
+                            ? Colors.blue
+                            : Colors.white,
+                    child: Text(
+                      "A",
+                      style: TextStyle(fontSize: 40.0),
+                    ),
+                  ),
+                ),
+              ),
+              
+          ExpansionTile(
+            title: Text("Filter"),
+            children: <Widget>[
+              _createDrawerItem(icon: FontAwesomeIcons.male, text: 'All',onTap: () => getProduct('All')),
+              _createDrawerItem(icon: FontAwesomeIcons.male, text: 'Men',onTap: () => getProduct('Men')),
+              _createDrawerItem(icon: FontAwesomeIcons.female, text: 'Women', onTap: () => getProduct('Women')),
+              _createDrawerItem(icon: FontAwesomeIcons.child, text: 'Kids', onTap: () => getProduct('Kid')),
+            ],
+          ),
+          Divider(),
+          _createDrawerItem(icon: FontAwesomeIcons.user, text: 'My Account'),
+          PlatformButton(
+            onPressed: (){},
+            child: PlatformText('LOG OUT'),
+            color: Color.fromRGBO(255, 0, 0, 1.0),
+            android: (_) => MaterialRaisedButtonData(),
+            ios: (_) => CupertinoButtonData()
+          )
+        ],
+      ),
+    ),
       appBar: AppBar(
           title: PlatformText("Resideo eShopping"),
-          actions: <Widget> [
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 10),
-              height: 100,
-              width: 100,
-              child: dropdownWidget(),
-              
-            )
-          ]),
+          ),
       body: widget,
     );
   }
 
+/*
   Widget dropdownWidget() {
     return DropdownButton(
       icon: Icon(Icons.filter_list),
       //hint: new Text("Filter"),
       items: [
-        DropdownMenuItem<String>(child: PlatformText("All"), value: "All"),
+        DropdownMenuItem<String>(child: PlatformText("All"), value: "All",),
         DropdownMenuItem<String>(child: PlatformText("Men"), value: "Men"),
         DropdownMenuItem<String>(child: PlatformText("Women"), value: "Women"),
         DropdownMenuItem<String>(child: PlatformText("Kid"), value: "Kid")
       ],
       onChanged: (String value) {
-        getProducts(value);
+        getProduct(value);
       },
       isExpanded: false,
+    );
+  }
+*/
+
+  Widget _createDrawerItem(
+      {IconData icon, String text, String value, GestureTapCallback onTap}) {
+    return ListTile(
+      title: Row(
+        children: <Widget>[
+          Icon(icon),
+          Padding(
+            padding: EdgeInsets.only(left: 8.0),
+            child: Text(text),
+          )
+        ],
+      ),
+      onTap: onTap,
     );
   }
 
@@ -117,18 +166,15 @@ class _ProductsListPageState extends State<ProductsListPage>
       }
     });
 
-    getProducts("All");
-  }
 
+    getProduct("All");
+  }
+ 
   void _showDialog() {
     showDialog(
       context: context,
       builder: (context) => AlertDialog(
-            title: PlatformText("Internet needed!"),
-            content: PlatformText("You may want to exit the app here"),
-            actions: <Widget>[
-              FlatButton(child: PlatformText("Cancel"), onPressed: () => Navigator.of(context).pop(false)),
-              FlatButton(child: PlatformText("Exit"), onPressed: () => Navigator.of(context).pop(true))],
+            title: Text("Internet connection is required!"),
           ),
     );
   }

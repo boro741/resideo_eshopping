953b1382c80b54425509f2592a4c4f9562ad4975
Harshit <Harshit-resideo@github.com>
2019-12-11 20:20:57 +0530

    Revert "Commit test"
    This reverts commit a5d178d8

--- Resideo_eshopping/resideo_eshopping/lib/main.dart
+++ Resideo_eshopping/resideo_eshopping/lib/main.dart
@@ -16,7 +16,6 @@ class MyApp extends StatelessWidget {
       ),
       //home: MyHomePage(title: 'Resideo e-Shopping'),
      // home: ProductsListPage(),
-
       home: RootPage(auth: Auth()),
     );
   }

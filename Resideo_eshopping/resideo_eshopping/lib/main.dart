0d2761cb8058af16c19dffbacd24826cd8eef980
VandanaBaidya <vandanabaidya@gmail.com>
2020-01-03 15:29:20 +0530

    Added MultiProvider

--- Resideo_eshopping/resideo_eshopping/lib/main.dart
+++ Resideo_eshopping/resideo_eshopping/lib/main.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:resideo_eshopping/Screens/home_page.dart';
+import 'package:resideo_eshopping/model/user_repository.dart';
+import 'package:resideo_eshopping/services/authentication.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Resideo e-Shopping',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      //home: MyHomePage(title: 'Resideo e-Shopping'),
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(builder: (context) => UserRepository.instance(),), ],
+      child: MaterialApp(
+        title: 'Resideo e-Shopping',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        //home: MyHomePage(title: 'Resideo e-Shopping'),
 
-      //home: RootPage(auth: Auth()),
-       home: HomePage(),
-    );
+        //home: RootPage(auth: Auth()),
+        home: HomePage(auth: Auth()),
+      ),
+
+      );
   }
 }

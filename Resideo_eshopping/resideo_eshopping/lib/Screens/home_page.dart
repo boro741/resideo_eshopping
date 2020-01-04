eca293a5e0cb1c61f5f37075c76725cbf82bf9ed
VandanaBaidya <vandanabaidya@gmail.com>
2020-01-05 01:12:09 +0530

    Updated cases for authentication status

--- Resideo_eshopping/resideo_eshopping/lib/Screens/home_page.dart
+++ Resideo_eshopping/resideo_eshopping/lib/Screens/home_page.dart
@@ -1,17 +1,12 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:provider/provider.dart';
-import 'package:resideo_eshopping/Screens/login_page.dart';
 import 'package:resideo_eshopping/Screens/product_list_page.dart';
 import 'package:resideo_eshopping/model/user_repository.dart';
-import 'package:resideo_eshopping/services/authentication.dart';
 import 'package:resideo_eshopping/stores/home_page_store.dart';
 import 'package:resideo_eshopping/widgets/progress_indicator.dart';
 
 class HomePage extends StatelessWidget{
-  HomePage({this.auth});
-  final BaseAuth auth;
 
   final _homeStore = HomePageStore();
 
@@ -25,20 +20,8 @@ class HomePage extends StatelessWidget{
 
               case Status.Unauthenticated:
               case Status.Authenticating:
-                return Observer(
-                  builder: (context){
-                    if(_homeStore.logInButtonPress){
-                      return LoginPage( onSignedIn: _homeStore.onLoggedIn);
-                    }
-                    else{
-                      return ProductsListPage(user: user.user, online: _homeStore.onlogInButtonPress, offline: _homeStore.onSignedOut);
-                    }
-
-                  },
-                );
-                break;
-
               case Status.Authenticated:
+
                   return Observer(
                     builder: (context) {
                       return ProductsListPage(

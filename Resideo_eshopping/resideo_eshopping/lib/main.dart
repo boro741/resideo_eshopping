1dfeac2507716e50a6bdd6c09699b2e6d8f2055d
VandanaBaidya <vandanabaidya@gmail.com>
2020-01-05 01:06:28 +0530

    Used providers

--- Resideo_eshopping/resideo_eshopping/lib/main.dart
+++ Resideo_eshopping/resideo_eshopping/lib/main.dart
@@ -2,11 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:resideo_eshopping/Screens/home_page.dart';
 import 'package:resideo_eshopping/model/user_repository.dart';
-import 'package:resideo_eshopping/services/authentication.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
@@ -16,10 +14,8 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        //home: MyHomePage(title: 'Resideo e-Shopping'),
 
-        //home: RootPage(auth: Auth()),
-        home: HomePage(auth: Auth()),
+        home: HomePage(),
       ),
 
       );

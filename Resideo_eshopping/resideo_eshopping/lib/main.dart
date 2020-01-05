953074bef62162ed98be35a15e349737026cb4e6
VandanaBaidya <vandanabaidya@gmail.com>
2020-01-05 15:16:43 +0530

    Localization feature

--- Resideo_eshopping/resideo_eshopping/lib/main.dart
+++ Resideo_eshopping/resideo_eshopping/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:resideo_eshopping/Screens/home_page.dart';
 import 'package:resideo_eshopping/model/user_repository.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:resideo_eshopping/controller/app_localizations.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -10,14 +12,34 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [ChangeNotifierProvider(builder: (context) => UserRepository.instance(),), ],
       child: MaterialApp(
-        title: 'Resideo e-Shopping',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-
-        home: HomePage(),
+      title: 'Resideo e-Shopping',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-
-      );
+      supportedLocales: [
+        const Locale('en','US'),
+        const Locale('sk','SK'),
+      ],
+      localizationsDelegates: [
+        // ... app-specific localization delegate[s] here
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      localeResolutionCallback: (locale, supportedLocales) {
+        // Check if the current device locale is supported
+        for (var supportedLocale in supportedLocales) {
+          if (supportedLocale.languageCode == locale.languageCode &&
+              supportedLocale.countryCode == locale.countryCode) {
+            return supportedLocale;
+          }
+        }
+        // If the locale of the device is not supported, use the first one
+        // from the list (English, in this case).
+        return supportedLocales.first;
+      },
+      home: HomePage(),
+    ),
+    );
   }
 }

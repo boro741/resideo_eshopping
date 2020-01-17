518d3fc1e2d2241cb59870393e11237d0856aa7f
Kadali, Prasanna <prasanna.kadali@resideo.com>
2020-01-17 11:07:14 +0530

    undo changes

--- Resideo_eshopping/resideo_eshopping/lib/Screens/image_picker_dialog.dart
+++ Resideo_eshopping/resideo_eshopping/lib/Screens/image_picker_dialog.dart
@@ -5,13 +5,7 @@ import 'package:resideo_eshopping/controller/image_picker_handler.dart';
 import 'package:flare_flutter/flare_actor.dart';
 import 'package:flare_flutter/flare_controls.dart';
 
-enum AnimationToPlay {
-  Activate,
-  Deactivate,
-  CameraTapped,
-  PulseTapped,
-  ImageTapped
-}
+
 
 class ImagePickerDialog extends StatelessWidget {
   final ImagePickerHandler _listener;
@@ -23,12 +17,8 @@ class ImagePickerDialog extends StatelessWidget {
       this._listener, this._controller, this._deletePhotoButtonEnable);
   Animation<double> _drawerContentsOpacity;
   Animation<Offset> _drawerDetailsPosition;
-  AnimationToPlay _animationToPlay = AnimationToPlay.Deactivate;
-  AnimationToPlay _lastPlayedAnimation;
   final FlareControls animationControls = FlareControls();
   bool isOpen = false;
-  static const double AnimationWidth = 295.0;
-  static const double AnimationHeight = 251.0;
 
   void initState() {
     _drawerContentsOpacity = new CurvedAnimation(
@@ -106,57 +96,12 @@ class ImagePickerDialog extends StatelessWidget {
         child: new Opacity(
           opacity: 1.0,
           child: new Container(
-            width: AnimationWidth,
-            height: AnimationHeight,
             padding: EdgeInsets.fromLTRB(30.0, 0.0, 30.0, 20.0),
             child: Column(
               mainAxisSize: MainAxisSize.max,
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 new GestureDetector(
-        //             onTapUp: (tapInfo) {
-        //               var localTouchPosition =
-        //                   (context.findRenderObject() as RenderBox)
-        //                       .globalToLocal(tapInfo.globalPosition);
-
-        //               var topHalfTouched =
-        //                   localTouchPosition.dy < AnimationHeight / 2;
-
-        //               var leftSideTouched =
-        //                   localTouchPosition.dx < AnimationWidth / 3;
-
-        //               var rightSideTouched =
-        //                   localTouchPosition.dx > (AnimationWidth / 3) * 2;
-
-        //               var middleTouched = !leftSideTouched && !rightSideTouched;
-        //               if (leftSideTouched && topHalfTouched) {
-        //                  _listener.openCamera();
-        //                 _setAnimationToPlay(AnimationToPlay.CameraTapped);
-                        
-        //               } else if (middleTouched && topHalfTouched) {
-        //                 _listener.removePicture();
-        //                 _setAnimationToPlay(AnimationToPlay.PulseTapped);
-        //               } else if (rightSideTouched && topHalfTouched) {
-        //                 _listener.openGallery();
-        //                 _setAnimationToPlay(AnimationToPlay.ImageTapped);
-        //               } else {
-        //                 if (isOpen) {
-        //                   _setAnimationToPlay(AnimationToPlay.Deactivate);
-        //                 } else {
-        //                   _setAnimationToPlay(AnimationToPlay.Activate);
-        //                 }
-
-        //                 isOpen = !isOpen;
-                        
-        //               }
-        //             },
-        //             child: FlareActor('assets/button-animation.flr',
-        //                 controller: animationControls,
-        //                 animation: 'deactivate')),
-        //       ],
-        //     ),
-        //   ),
-        // ));
 
             onTap: () => _listener.openCamera(),
             child: new CircleAvatar(
@@ -203,33 +148,4 @@ class ImagePickerDialog extends StatelessWidget {
     ),
     ));
   }
-
-  // String _getAnimationName(AnimationToPlay animationToPlay) {
-  //   switch (animationToPlay) {
-  //     case AnimationToPlay.Activate:
-  //       return 'activate';
-  //     case AnimationToPlay.Deactivate:
-  //       return 'deactivate';
-  //     case AnimationToPlay.CameraTapped:
-  //       return 'camera_tapped';
-  //     case AnimationToPlay.PulseTapped:
-  //       return 'pulse_tapped';
-  //     case AnimationToPlay.ImageTapped:
-  //       return 'image_tapped';
-  //     default:
-  //       return 'deactivate';
-  //   }
-  // }
-
-  // void _setAnimationToPlay(AnimationToPlay animation) {
-  //   var isTappedAnimation = _getAnimationName(animation).contains("_tapped");
-  //   if (isTappedAnimation &&
-  //       _lastPlayedAnimation == AnimationToPlay.Deactivate) {
-  //     return;
-  //   }
-
-  //   animationControls.play(_getAnimationName(animation));
-
-  //   _lastPlayedAnimation = animation;
-  // }
 }

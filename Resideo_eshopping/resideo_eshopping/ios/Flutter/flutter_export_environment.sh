5da89536b04b8383e485ed72f404d259f5229209
komal655 <komal.singh@resideo.com>
2020-01-21 13:19:16 +0530

    added catcher dependency

--- Resideo_eshopping/resideo_eshopping/ios/Flutter/flutter_export_environment.sh
+++ Resideo_eshopping/resideo_eshopping/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/harshitagrawal/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/harshitagrawal/Documents/GitHub/resideo_eshopping/Resideo_eshopping/resideo_eshopping"
-export "FLUTTER_TARGET=/Users/harshitagrawal/Documents/GitHub/resideo_eshopping/Resideo_eshopping/resideo_eshopping/lib/main.dart"
+export "FLUTTER_ROOT=/Users/singhkomal/Downloads/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/singhkomal/resideo_eshopping/Resideo_eshopping/resideo_eshopping"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/harshitagrawal/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/singhkomal/Downloads/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
